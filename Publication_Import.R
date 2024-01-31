0dc0f791b61c3db65c60d21fd04dc3dc23be7302
Jason Everett <jaseeverett@gmail.com>
2024-02-01 08:03:08 +1100

    Website update

--- Publication_Import.R
+++
@@ -1,6 +0,0 @@
-
-#devtools::install_github("petzi53/bib2academic", build_vignettes = TRUE)
-
-library(bib2academic)
-bib2acad("Export.bib", copybib = TRUE, abstract = TRUE, overwrite = TRUE)
-
--- content/home/index.md
+++
@@ -1,5 +0,0 @@
----
-headless: true
-type: widget_page
----
-
